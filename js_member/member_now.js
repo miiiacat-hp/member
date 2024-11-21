1a1b854cf07e8ac9e919bcf102d1df1628618ecd
miiiacat-hp <104990744+miiiacat-hp@users.noreply.github.com>
2024-11-22 02:19:16 +0900

    Update member_now.js

--- js_member/member_now.js
+++ js_member/member_now.js
@@ -46,8 +46,8 @@ function member_list(dai){
 	    num = 22;
 	    break;
 	case 7:
-	    pass = ['asagi','atamin','orumu','komugi','koremitu','sion','sei','soreiyu','tuyu','pikorin','foron','beru','pomerin','ravin','rate','riri-','ruxuna','rein','rebekka','romi','romio'];    
-            name = ['あさぎ','あたみん','おるむ','こむぎ','文秀','詩音','犀','ソレイユ','つゆ','ぴこりん','ふぉろん','べる','ぽめりん','らゔぃん','ラテ','リリー','るぅな','れいん','レベッカ','ロミ','ろみお'];
+	    pass = ['asagi','atamin','orumu','komugi','koremitu','sion','sei','soreiyu','tuyu','pipi','pikorin','foron','beru','pomerin','ravin','rate','riri-','ruxuna','rein','rebekka','romi','romio'];    
+            name = ['あさぎ','あたみん','おるむ','こむぎ','文秀','詩音','犀','ソレイユ','つゆ','ぴこりん','ぴぴ','ふぉろん','べる','ぽめりん','らゔぃん','ラテ','リリー','るぅな','れいん','レベッカ','ロミ','ろみお'];
 	    shozoku = 'th7';
 	    num = 21;
 	default:
